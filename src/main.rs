39a1d35ca88edddac470038f80cdf8c95ab672ec
hkohko <hekodev.py@gmail.com>
2023-12-30 11:20:36 +0700

    tweaked main.rs to reflect changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use fzmovies::core::client::client;
-use fzmovies::download_link::{build_url, db, scraper};
+use fzmovies::file_quality::{build_url, db, scraper};
 use fzmovies::movie_page::{build_url::build_url, db::db_main, scraper::scp};
 
 fn main() {
@@ -8,15 +8,15 @@ fn main() {
 fn download_link_scraper() {
     let read = db::db_main();
     let urls = match read {
-        Ok(val) => build_url::build_url(val),
+        Ok(val) => build_url::build_url(&val),
         Err(e) => panic!("{e}"),
     };
     let resp = match urls {
-        Ok(val) => client(&val),
+        Ok(val) => client(&val, true, Some("quality.html")),
         Err(e) => panic!("{}", e),
     };
     match resp {
-        Ok(val) => scraper::scp_link(val.as_str()),
+        Ok(val) => scraper::scp_main(val.as_str()),
         Err(e) => panic!("{e}"),
     }
 }
@@ -26,7 +26,7 @@ fn movie_page_scraper() {
         println!("page: {}", &page);
         let url = build_url("Action", "downloads", page.as_str());
         let resp = match url {
-            Ok(val) => client(&val),
+            Ok(val) => client(&val, false, None),
             Err(e) => panic!("{e}"),
         };
         let data = match resp {
